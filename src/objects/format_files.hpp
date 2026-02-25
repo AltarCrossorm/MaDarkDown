d8c7ee2b33b0abe564ba4903c836d70347b62ed4
Enguerran Cresp <enguerran.cresp@etudiant.univ-lr.fr>
2026-02-25 15:44:21 +0100

    (MarkDown) Created file to manage objects integration

---
+++ src/objects/format_files.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "base.hpp"
+#include "header.hpp"
+#include "subtext.hpp"
+#include "codeblock.hpp"
+#include "note.hpp"
+#include "colored_note.hpp"
+#include "table.hpp"
+#include "ordered_list.hpp"
+#include "unordered_list.hpp"
+#include "checkbox.hpp"
